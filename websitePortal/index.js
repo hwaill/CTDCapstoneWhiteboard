43117a4bafe39f444c5c9c5a49441877fccd2817
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2024-02-21 13:55:13 -0700

    js for webportal created

---
+++ websitePortal/index.js
@@ -0,0 +1,7 @@
+var todoCheckBox = document.getElementById("Todo-Check");
+var weeklyCalCheckBox = document.getElementById("WeeklyCal-Check");
+var quotesCheckBox = document.getElementById("Quotes-Check");
+var weatherCheckBox = document.getElementById("Weather-Check");
+var gameCheckBox = document.getElementById("Game-Check");
+var moodCheckBox = document.getElementById("Mood-Check");
+var themeCheckBox = document.getElementById("Theme-Check");
