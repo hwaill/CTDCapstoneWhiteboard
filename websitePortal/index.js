52435bc6e183d216f6f531c2ad3d53cc251a10b6
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2024-02-21 15:14:16 -0700

    string values for features checked

--- websitePortal/index.js
+++ websitePortal/index.js
@@ -5,3 +5,20 @@ var weatherCheckBox = document.getElementById("Weather-Check");
 var gameCheckBox = document.getElementById("Game-Check");
 var moodCheckBox = document.getElementById("Mood-Check");
 var themeCheckBox = document.getElementById("Theme-Check");
+
+var featureSelectedString;
+
+function featureSelected() {
+  
+    var todoValue = todoCheckBox.checked ? "1" : "0";
+    var weeklyCalValue = weeklyCalCheckBox.checked ? "1" : "0";
+    var quotesValue = quotesCheckBox.checked ? "1" : "0";
+    var weatherValue = weatherCheckBox.checked ? "1" : "0";
+    var gameValue = gameCheckBox.checked ? "1" : "0";
+    var moodValue = moodCheckBox.checked ? "1" : "0";
+    var themeValue = themeCheckBox.checked ? "1" : "0";
+
+    featureSelectedString = todoValue + weeklyCalValue + quotesValue + weatherValue + gameValue + moodValue + themeValue;
+
+    console.log(featureSelectedString);
+}
